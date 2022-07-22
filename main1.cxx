85298e191592657f19406aef48495977b05b3c41
Alexey <gluchov44@gmail.com>
2022-07-22 11:38:24 +0300

    ans2

--- main1.cxx
+++ main1.cxx
@@ -1,22 +1,25 @@
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-long primt_f(long n) {
-  if (n % 2 == 0)
-    return 2;
-  int i = 3;
-  while (n % i != 0 && i * i <= n) {
-    i += 2;
-  }
-  if (i * i <= n) {
-    return i;
-  }
-  return n;
-}
-
 int main() {
-  int number;
-  cin >> number;
-  cout << primt_f(number);
+  int n;
+  cin >> n;
+  int s = 0;
+  int c = 0;
+  int i = 1;
+  for (; pow(i, 2) <= n;) {
+    if (n % i <= 0) {
+      if (pow(i, 2) == n) {
+        c++;
+        s += i;
+      } else {
+        c += 2;
+        s += i + n / i;
+      }
+      i++;
+    }
+  }
+  cout << c << " " << s;
 }
